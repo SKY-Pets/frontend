9bb27f385afbdfb074f83b8f6a28e070b2dd8fec
Matijasevic-Emer <matijasevic.emerson@gmail.com>
2025-04-09 13:59:21 -0300

    c

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -6,7 +6,7 @@ const ProductList = ({ products }) => {
     <Box>
       <Grid container spacing={3} alignItems="stretch">
         {products.map((product) => (
-          <Grid item xs={12} sm={6} md={4} key={product.id}>
+          <Grid item xs={12} sm={6} md={6} key={product.id}>
             <ProductItem product={product} />
           </Grid>
         ))}
